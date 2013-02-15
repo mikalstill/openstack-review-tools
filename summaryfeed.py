8878524896d352562ce6bd46b4934cfb2bf2557e
Michael Still <mikal@stillhq.com>
2013-02-15 23:10:56 +0000

    Selectable users and groups.

--- summaryfeed.py
+++ summaryfeed.py
@@ -2,6 +2,7 @@
 
 # Take gerrit status feeds and turn them into an RSS feed
 
+import cgi
 import datetime
 import json
 import random
@@ -26,6 +27,11 @@ if __name__ == '__main__':
     print '\r'
     sys.stdout.flush()
 
+    showusers = ['mikalstill']
+    form = cgi.FieldStorage()
+    if form.has_key('reviewers'):
+      showusers = form['reviewers'].value.lstrip(' ').split(' ')
+
     db = MySQLdb.connect(user = flags['dbuser'],
                          db = flags['dbname'],
                          passwd = flags['dbpassword'])
@@ -34,15 +40,34 @@ if __name__ == '__main__':
     # Fetch the last seven days of results to start off with
     last_time = 0
     initial_size = 30
+    one_day = datetime.timedelta(days=1)
+
+    start_of_window = datetime.datetime.now()
+    start_of_window -= one_day * initial_size
+
+    groups = []
+    cursor.execute('select * from groups;')
+    for row in cursor:
+      groups.append([row['name'], row['members'].split(' ')])
+    SendPacket({'type': 'groups',
+                'payload': groups})
+
+    all_reviewers = []
+    cursor.execute('select distinct(username), max(day) from summary '
+                   'where day > date(%s) group by username;'
+                   % sql.FormatSqlValue('timestamp', start_of_window))
+    for row in cursor:
+      all_reviewers.append((row['username'], row['max(day)'].isoformat()))
+    SendPacket({'type': 'users-all',
+                'payload': all_reviewers})
 
     SendPacket({'type': 'users-present',
-                'payload': ['mikalstill', 'lifeless', 'devananda']})
+                'payload': showusers})
 
-    for username in ['mikalstill', 'lifeless', 'devananda']:
+    for username in showusers:
         day = datetime.datetime.now()
         day = datetime.datetime(day.year, day.month, day.day)
 
-        one_day = datetime.timedelta(days=1)
         day -= one_day * (initial_size - 1)
         for i in range(initial_size):
             timestamp = sql.FormatSqlValue('timestamp', day)
@@ -80,9 +105,10 @@ if __name__ == '__main__':
         # Now check for updates
         SendPacket({'type': 'keepalive'})
         SendPacket({'type': 'debug',
-                    'payload': 'Querying for updates after %d' % last_time})
+                    'payload': ('Querying for updates after %d, server time %s'
+                                %(last_time, datetime.datetime.now()))})
 
-        for username in ['mikalstill', 'lifeless', 'devananda']:
+        for username in showusers:
             cursor.execute('select * from summary where username="%s" and '
                            'epoch > %d;'
                            %(username, last_time))
