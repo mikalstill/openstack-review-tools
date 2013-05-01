30adc66233eb64f49c520e57a783cc81be799f09
Michael Still <mikal@stillhq.com>
2013-05-01 00:05:54 +0000

    Track stackforge projects.

--- scrapereviews_getprojects.sh
+++ scrapereviews_getprojects.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for item in `wget -O - "https://raw.github.com/openstack-infra/config/master/modules/openstack_project/templates/review.projects.yaml.erb" 2> /dev/null | egrep "^- project" | grep "openstack" | sed 's/- project: //'`
+for item in `wget -O - "https://raw.github.com/openstack-infra/config/master/modules/openstack_project/templates/review.projects.yaml.erb" 2> /dev/null | egrep "^- project" | egrep "(openstack|stackforge)" | sed 's/- project: //'`
 do
   echo "    Reviews('$item')"
 done
