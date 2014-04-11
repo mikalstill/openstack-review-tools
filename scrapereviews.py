36aeb279b0362b1b9e0470b4cffa594286e6a25f
Michael Still <mikal@stillhq.com>
2014-04-11 14:00:00 +1000

    New scrapereviews which uses the gerrit event stream log

--- scrapereviews.py
+++ scrapereviews.py
@@ -10,111 +10,65 @@ import re
 import sys
 import time
 import MySQLdb
+import urllib
 
 import dbcachingexecute
 import feedutils
 import sql
 
 
-def Reviews(component):
+def Reviews(day):
     cursor = feedutils.GetCursor()
-    for l in dbcachingexecute.Execute(time.time() - 60,
-                                      'gerrit_query_approvals_json',
-                                      ('ssh -i ~/.ssh/id_gerrit '
-                                       'review.openstack.org gerrit query '
-                                       'project:%s '
-                                       '--all-approvals --patch-sets '
-                                       '--format JSON'),
-                                      component, cleanup=True):
 
-      try:
-          d = json.loads(l)
-      except:
-          continue
+    url = ('http://www.rcbops.com/gerrit/merged/%04d/%d/%d_reviews.json'
+           % (day.year, day.month, day.day))
+    print url
 
-      if d.has_key('id'):
-          b64 = base64.encodestring(l)
-          checksum = hashlib.sha1(l).hexdigest()
-          last_updated = datetime.datetime.fromtimestamp(d['lastUpdated'])
-          timestamp = sql.FormatSqlValue('timestamp', last_updated)
-          insert = ('insert ignore into changes (changeid, timestamp, '
-                    'parsed, checksum) values ("%s", %s, "%s", "%s");'
-                    %(d['id'], timestamp, b64, checksum))
-          cursor.execute(insert)
-          if cursor.rowcount == 0:
-              cursor.execute('select * from changes where changeid="%s";'
-                             % d['id'])
-              stored_checksum = cursor.fetchone()['checksum']
-              if checksum != stored_checksum:
-                  cursor.execute('delete from changes where changeid="%s";'
-                                 % d['id'])
-                  cursor.execute(insert)
-          cursor.execute('commit;')
+    remote = urllib.urlopen(url)
+    d = json.loads(''.join(remote.readlines()))
+    
+    for l in remote.readlines():
+        try:
+            d = json.loads(l)
+        except:
+            continue
 
-      for ps in d.get('patchSets', {}):
-          patchset = ps.get('number')
+    for reviewer in d:
+        for review in d['reviewer']:
+            cursor.execute('select * from reviews where changeid="%s" and '
+                           'username="%s" and component="%s" and '
+                           'patchset=%s and score=%s;'
+                           %(review['id'], reviewer, review['project'],
+                             review['patchset'], review['value']))
+            if cursor.rowcount == 0:
+                cursor.execute('insert into reviews (changeid, username, '
+                               'component, patchset, score, timestamp, day) '
+                               'values("%s", "%s", "%s", %s, %s, now(), '
+                               'date(now));')
+                cursor.execute('commit;')
 
-          for review in ps.get('approvals', []):
-              # Deliberately leave the timezone alone here so its consistant
-              # with reports others generate.
-              updated_at = datetime.datetime.fromtimestamp(
-                  review['grantedOn'])
-              username = review['by'].get('username', 'unknown')
+                summary = {}
+                cursor.execute('select * from reviews where '
+                               'username = "%s" and '
+                               'date(timestamp) = date(now()) '
+                               'order by timestamp asc;'
+                               % reviewer)
+                for review in cursor:
+                    summary.setdefault(review['component'], 0)
+                    summary[review['component']] += 1
+                    summary['__total__'] += 1
 
-              if username in ['jenkins', 'smokestack']:
-                  continue
-
-              timestamp = sql.FormatSqlValue('timestamp', updated_at)
-              score = review.get('value', 0)
-              cursor.execute('insert ignore into reviews '
-                             '(changeid, username, timestamp, day, '
-                             'component, patchset, score) '
-                             'values ("%s", "%s", %s, date(%s), "%s", %s, '
-                             '%s);'
-                             %(d['id'], username, timestamp, timestamp,
-                               component, patchset, score))
-              if cursor.rowcount > 0:
-                  # This is a new review, we assume we're the only writer
-                  print 'New review from %s' % username
-                  cursor.execute('select * from reviewsummary where '
-                                 'username="%s" and day=date(%s);'
-                                 %(username, timestamp))
-                  if cursor.rowcount > 0:
-                      row = cursor.fetchone()
-                      try:
-                          summary = json.loads(row['data'])
-                      except Exception, e:
-                          print ('Could not decode summary "%s": %s'
-                                 %(row['data'], e))
-                          summary = {}
-                  else:
-                      summary = {}
-
-                  summary.setdefault(component, 0)
-                  summary.setdefault('__total__', 0)
-                  summary[component] += 1
-                  summary['__total__'] += 1
-
-                  cursor.execute('delete from reviewsummary where '
-                                 'username="%s" and day=date(%s);'
-                                 %(username, timestamp))
-                  cursor.execute('insert into reviewsummary'
-                                 '(day, username, data, epoch) '
-                                 'values (date(%s), "%s", \'%s\', %d);'
-                                 %(timestamp, username,
-                                   json.dumps(summary),
-                                   int(time.time())))
-
-              cursor.execute('commit;')
-
+                epoch = time.mktime(review['timestamp'].timetuple())
+                cursor.execute('delete from reviewsummary where '
+                               'username="%s" and day=date(%s);'
+                               %(user_row['username'], timestamp))
+                cursor.execute('insert into reviewsummary'
+                               '(day, username, data, epoch) '
+                               'values (date(%s), "%s", \'%s\', %d);'
+                               %(timestamp, reviewer,
+                                 json.dumps(summary), epoch))
+                cursor.execute('commit;')
 
 if __name__ == '__main__':
-    for l in dbcachingexecute.Execute(time.time() - 60,
-                                      'gerrit_projects',
-                                      ('ssh -i ~/.ssh/id_gerrit '
-                                       'review.openstack.org gerrit %s'),
-                                      'ls-projects', cleanup=True):
-        l = l.rstrip()
-        print 'Project: %s' % l
-        Reviews(l)
+    Reviews(datetime.datetime.now())
 
